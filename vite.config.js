cba4efab81f1370d99d8bbf08cd95ceaebb1ab40
pambos777 <chrpamboss@gmail.com>
2024-12-18 10:24:04 +0200

    comm

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/CharalambosPortfolioFin",
-=======
-  base: "/charalambos-portfolio",
->>>>>>> 0373a95b57af187dbfc2f1cb637b94cba0d245e4
 });
